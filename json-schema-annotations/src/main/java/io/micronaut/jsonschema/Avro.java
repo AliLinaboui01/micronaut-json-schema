4ad5a8be1211e748ed6db05100e89e8271311580
AliLinaboui01 <alilinaboui@gmail.com>
2025-04-10 11:00:44 +0100

    avro annotation

---
+++ json-schema-annotations/src/main/java/io/micronaut/jsonschema/Avro.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017-2024 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.jsonschema;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * An annotation that signifies that json schema should be created for the object.
+ * The JSON schema will attempt to mimic the way this object would be serialized.
+ *
+ * @since 1.0.0
+ * @author Andriy Dmytruk
+ */
+@Target({ ElementType.TYPE, ElementType.FIELD })
+public @interface Avro {
+
+    /**
+     * The Name of the AVRO schema.
+     * By default, the class name will be used.
+     *
+     * @return The name
+     */
+    String name() default "";
+
+    /**
+     * The Doc of the AVRO schema.
+     * A JSON string providing documentation to the user of this schema
+     * By default, NONE
+     *
+     * @return The doc
+     */
+    String doc() default "";
+
+    /**
+     * The Namespace of the AVRO schema.
+     * A JSON string that qualifies the name
+     *
+     * @return The namespace
+     */
+    String namespace() default "";
+
+    /**
+     * Aliases for the schema.
+     *
+     * @return The aliases
+     */
+    String[] aliases() default {};
+
+}
